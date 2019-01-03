e17aba208f30b5438069d59c51c9314ace1a9afd
paroonk <paroonk@hotmail.com>
2019-01-03 15:13:25 +0700

    Regression

--- Regression.py
+++ Regression.py
@@ -20,19 +20,26 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 print(df.shape)
 df.dropna(inplace=True)
 print(df.shape)
-print(df.head())
+# print(df.head())
 
 X = np.array(df.drop(['label'], axis=1))
+X = preprocessing.scale(X)
 y = np.array(df['label'])
 
-print(X.shape)
-print(y.shape)
+print(X.shape, y.shape)
 
-print(X)
-X = preprocessing.scale(X)
-print(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+# clf = LinearRegression()
+clf = svm.SVR(kernel='poly')
+clf.fit(X_train, y_train)
+
+accuracy = clf.score(X_test, y_test)
+print(accuracy)
